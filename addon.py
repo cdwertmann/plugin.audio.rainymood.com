a963e37d9b4529b432dcb3efa89a91fa349d28f2
sheikr <sheikr@users.noreply.github.com>
2015-11-26 04:00:25 +0000

    Update addon.py
    Fix loop with Aeon Nox

--- addon.py
+++ addon.py
@@ -3,8 +3,7 @@ import xbmc,xbmcgui,xbmcplugin,xbmcaddon
 
 addon = xbmcaddon.Addon()
 img = xbmc.translatePath( os.path.join( addon.getAddonInfo('path'), 'rain.jpg' ) )
-url = "http://174.36.223.28/audio1110/classic.ogg"
-#url = "http://images.wikia.com/starwars/images/f/f5/A_little_short.ogg"
+url = "http://rainymood.com/audio1110/0.ogg"
 
 class MyPlayer(xbmc.Player):
   def __init__(self):
@@ -14,23 +13,21 @@ class MyPlayer(xbmc.Player):
     self.playNoise()
 
   def playNoise(self):
-    listitem = xbmcgui.ListItem('Rainy Mood')
-    self.play(url, listitem, False)
+    self.play(url)
 
 class MyWindow(xbmcgui.WindowDialog):
   def __init__(self):
     self.addControl(xbmcgui.ControlImage(1, 1, 1280, 720, img))
 
   def onAction(self, action):
-    # xbmc.log(str(action.getId()))
+    xbmc.log(str(action.getId()))
     # 10 = ACTION_PREVIOUS_MENU, 13 = ACTION_STOP, 93 = ACTION_NAV_BACK
     # see https://github.com/xbmc/xbmc/blob/master/xbmc/input/Key.h
     if action==10 or action==13 or action==92:
-      p.stop()
+      xbmc.executebuiltin("PlayerControl(Stop)")
       self.close()
 
 p = MyPlayer()
 w = MyWindow()
-xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=True )
 w.doModal()
 del w
